641f2b566971afafeb594f95f665516c412d754c
SupunArunoda <supunarunoda@gmail.com>
2018-06-08 00:50:38 +0530

    Added lexiography feature

--- org.eclipse.cpacep/src/org/eclipse/cpacep/util/FileHandler.java
+++ org.eclipse.cpacep/src/org/eclipse/cpacep/util/FileHandler.java
@@ -76,7 +76,7 @@ public class FileHandler {
 			@Override
 			public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
 				if (pathMatcher.matches(path)) {
-					String tempPath = (path.toString().replaceAll(location + "/", "")).toLowerCase();
+					String tempPath = StringHandler.toCamelCase((path.toString().replaceAll(location + "/", "")).toLowerCase());
 
 					if (glob.contains(CPACEPConnector.SPEC_FILE_TYPE)) {
 						matchedList.add(tempPath.replaceAll(CPACEPConnector.SPEC_FILE_TYPE, ""));
@@ -92,7 +92,7 @@ public class FileHandler {
 				return FileVisitResult.CONTINUE;
 			}
 		});
-		Collections.sort(matchedList);
+		Collections.sort(matchedList, String.CASE_INSENSITIVE_ORDER);
 		return matchedList;
 	}
 }
