5a8020ea3d8bcf756544fcc2b1fff26687752f1d
SupunArunoda <supunarunoda@gmail.com>
2018-06-09 11:07:06 +0530

    Fixed #4 v3

--- org.eclipse.cpacep/src/org/eclipse/cpacep/util/FileHandler.java
+++ org.eclipse.cpacep/src/org/eclipse/cpacep/util/FileHandler.java
@@ -76,7 +76,7 @@ public class FileHandler {
 			@Override
 			public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
 				if (pathMatcher.matches(path)) {
-					String tempPath = StringHandler.toCamelCase((path.toString().replaceAll(location + "/", "")).toLowerCase());
+					String tempPath = (path.toString().replaceAll(location + "/", ""));
 
 					if (glob.contains(CPACEPConnector.SPEC_FILE_TYPE)) {
 						matchedList.add(tempPath.replaceAll(CPACEPConnector.SPEC_FILE_TYPE, ""));
