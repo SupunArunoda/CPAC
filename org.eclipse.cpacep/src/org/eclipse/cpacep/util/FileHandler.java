SupunArunoda/CPAC

commit 888867c602e6969d278f2f7f141004c3bd7d1b38

Use Path instead of Strings to avoid OS-specifics.
Also simplifies some methods regarding path handling and globbing.

org.eclipse.cpacep/src/org/eclipse/cpacep/util/FileHandler.java
@@ -13,21 +13,22 @@ public class FileHandler {
 		return Files.createTempDirectory("cpacep").toFile();
 	}
 
-	public static ArrayList<String> fileMatcher(String glob, String location) throws IOException {
+	public static ArrayList<String> fileMatcher(String glob, Path location) throws IOException {
 		ArrayList<String> matchedList = new ArrayList<String>();
 		final PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher(glob);
 
-		Files.walkFileTree(Paths.get(location), new SimpleFileVisitor<Path>() {
+		Files.walkFileTree(location, new SimpleFileVisitor<Path>() {
 
 			@Override
 			public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-				if (pathMatcher.matches(path)) {
-					String tempPath = (path.toString().replaceAll(location + File.separator, "")).toLowerCase();
+				Path fileName = path.getFileName();
+				if (pathMatcher.matches(fileName)) {
+					String tempPath = fileName.toString();
 
 					if (glob.contains(CPACEPConnector.SPEC_FILE_TYPE)) {
-						matchedList.add(tempPath.replaceAll(CPACEPConnector.SPEC_FILE_TYPE, ""));
+						matchedList.add(tempPath.replace(CPACEPConnector.SPEC_FILE_TYPE, ""));
 					} else if (glob.contains(CPACEPConnector.CONFIG_FILE_TYPE)) {
-						matchedList.add(tempPath.replaceAll(CPACEPConnector.CONFIG_FILE_TYPE, ""));
+						matchedList.add(tempPath.replace(CPACEPConnector.CONFIG_FILE_TYPE, ""));
 					}
 				}
 				return FileVisitResult.CONTINUE;

org.eclipse.cpacep.ui/src/org/eclipse/internal/cpacep/launcher/MainLaunchingTab.java
@@ -10,6 +10,8 @@ import org.eclipse.swt.layout.RowLayout;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import org.eclipse.core.resources.IResource;
@@ -242,13 +244,14 @@ public class MainLaunchingTab extends AbstractLaunchConfigurationTab {
     }
 
     private void updateComboBox() {
-	if (!isUpdateComboData) {
-	    String homeDir = StringHandler.getHomePath(executableText.getText().trim(), CPACEPConnector.CPA_HOME_PATH);
+	if (!isUpdateComboData && !executableText.getText().isEmpty()) {
+	    Path homeDir = StringHandler.getHomePath(executableText.getText().trim());
+	    Path configDir = homeDir.resolve("config");
 	    try {
-		specificationData = FileHandler.fileMatcher("glob:**" + File.separator + "*.spc",
-			homeDir + File.separator + "config" + File.separator + "specification");
-		configurationData = FileHandler.fileMatcher("glob:**" + File.separator + "*.properties",
-			homeDir + File.separator + "config");
+		specificationData = FileHandler.fileMatcher("glob:*.spc",
+			configDir.resolve("specification"));
+		configurationData = FileHandler.fileMatcher("glob:*.properties",
+			configDir);
 		if (specificationData != null) {
 		    specificationCombo.setEnabled(true);
 

org.eclipse.cpacep/src/org/eclipse/cpacep/util/CPACEPConnector.java
@@ -54,7 +54,7 @@ public class CPACEPConnector {
 	}
 
 	public void setCPACheckerHome() {
-		CPACheckerHome = StringHandler.getHomePath(lcCPACEPExecutable, CPA_HOME_PATH);
+		CPACheckerHome = StringHandler.getHomePath(lcCPACEPExecutable).toString();
 	}
 
 	public void setOutputDirectory() throws IOException {

org.eclipse.cpacep/src/org/eclipse/cpacep/util/StringHandler.java
@@ -1,18 +1,11 @@
 package org.eclipse.cpacep.util;
 
-import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class StringHandler {
 
-	public static String getHomePath(String path, int gap) {
-		String homeSplit[] = path.split(File.separator);
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < homeSplit.length - gap; i++) {
-			if (!homeSplit[i].equals("")) {
-				sb.append(File.separator + homeSplit[i]);
-			}
-		}
-		return sb.toString();
+	public static Path getHomePath(String pExecutable) {
+		return Paths.get(pExecutable).getParent().getParent();
 	}
-
 }

commit 5191fe2a29985f0689acce2f76fe5de32eecb610

Keep relative paths in spec and config dropdown
Fixes a bug introduced in 888867c that only showed the filename without
suffix.

org.eclipse.cpacep/src/org/eclipse/cpacep/util/FileHandler.java
@@ -23,7 +23,7 @@ public class FileHandler {
 			public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
 				Path fileName = path.getFileName();
 				if (pathMatcher.matches(fileName)) {
-					String tempPath = fileName.toString();
+					String tempPath = location.relativize(path).toString();
 
 					if (glob.contains(CPACEPConnector.SPEC_FILE_TYPE)) {
 						matchedList.add(tempPath.replace(CPACEPConnector.SPEC_FILE_TYPE, ""));