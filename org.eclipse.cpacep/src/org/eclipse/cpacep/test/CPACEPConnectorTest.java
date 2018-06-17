17a12ea3e7005750e505ac3fc8e32ba506dfdb07
SupunArunoda <supunarunoda@gmail.com>
2018-06-17 18:34:55 +0530

    Added setup method before Test

--- org.eclipse.cpacep/src/org/eclipse/cpacep/test/CPACEPConnectorTest.java
+++ org.eclipse.cpacep/src/org/eclipse/cpacep/test/CPACEPConnectorTest.java
@@ -3,6 +3,7 @@ package org.eclipse.cpacep.test;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.IOException;
 import org.eclipse.cpacep.util.CPACEPConnector;
 import org.eclipse.cpacep.util.FileHandler;
 import org.junit.Before;
@@ -15,19 +16,27 @@ public class CPACEPConnectorTest {
 	StringBuilder sb;
 	String lcSpecification;
 	String lcConfiguration;
-	String outputPathTest = "/tmp/test";
+	String result;
+
 	String outputPathOriginal = "/tmp/original";
+	File outputDirectory;
 
 	@Before
 	public void setup() {
 		lcSpecification = "default";
 		lcConfiguration = "default";
+		try {
+			outputDirectory = FileHandler.createTempDirectory();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		sb = new StringBuilder();
 		sb.append(homePath + File.separator + "scripts" + File.separator + "cpa.sh");
 		sb.append(" -spec " + homePath + File.separator + "config" + File.separator + "specification" + File.separator + lcSpecification + ".spc");
 		sb.append(" -config " + homePath + File.separator + "config" + File.separator + lcConfiguration + ".properties");
 		sb.append(" " + homePath + File.separator + "doc" + File.separator + "examples" + File.separator + "example.c");
-		sb.append(" -outputpath " + outputPathTest);
+		sb.append(" -outputpath " + outputDirectory);
 		sb.append(" -stats");
 		CPACEPConnector.executeCommand(sb.toString());
 
@@ -35,8 +44,8 @@ public class CPACEPConnectorTest {
 
 	@Test
 	public void testStatistics() {
-
-		String actualData = FileHandler.readFile(outputPathTest + File.separator + "Statistics.txt");
+		setup();
+		String actualData = FileHandler.readFile(outputDirectory.getAbsolutePath() + File.separator + "Statistics.txt");
 		String expectData = FileHandler.readFile(outputPathOriginal + File.separator + "Statistics.txt");
 		assertEquals(expectData, actualData);
 
