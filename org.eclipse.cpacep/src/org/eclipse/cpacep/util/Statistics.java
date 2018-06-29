384bb252b37efdc7343fa4b72efb01a9d96878ea
SupunArunoda <supunarunoda@gmail.com>
2018-06-30 01:28:38 +0530

    Removed the error prones when creating stat object

--- org.eclipse.cpacep/src/org/eclipse/cpacep/util/Statistics.java
+++ org.eclipse.cpacep/src/org/eclipse/cpacep/util/Statistics.java
@@ -1,31 +1,47 @@
 package org.eclipse.cpacep.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Statistics {
 
-  private String header;
-  private List<String> body;
+	private String header;
+	private List<String> body;
 
-  public Statistics(String header, List<String> body) {
-    this.header = header;
-    this.body = body;
-  }
+	public Statistics() {
+		this.body = new ArrayList<String>();
+	}
 
-  public void setHeader(String header) {
-    this.header = header;
-  }
+	public void addLine(String line) {
+		this.body.add(line);
+	}
 
-  public String getHeader() {
-    return header;
-  }
+	public void removeLastLine() {
+		this.body.remove(body.size() - 1);
+	}
 
-  public void setBody(List<String> body) {
-    this.body = body;
-  }
+	public void removeHeaderFromBody() {
+		body.remove(header);
+	}
 
-  public List<String> getBody() {
-    return body;
-  }
+	public String getLastAddedLine() {
+		return body.get(body.size() - 1);
+	}
+
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+	public String getHeader() {
+		return header;
+	}
+
+	public void setBody(List<String> body) {
+		this.body = body;
+	}
+
+	public List<String> getBody() {
+		return body;
+	}
 
 }
